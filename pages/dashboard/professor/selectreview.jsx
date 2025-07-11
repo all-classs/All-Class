9c98af0e8d67ccaf070eabcf7317d9b4c2637bfb
solp721 <easter721@gmail.com>
2025-07-11 13:42:28 +0900

    init : 초기화

--- pages/dashboard/professor/selectreview.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function selectreview() {
-	return <div>selectreview</div>;
-}
--- stores/useSignupModalStore.js
+++
@@ -1,9 +0,0 @@
-import { create } from 'zustand';
-
-const useSignupModalStore = create(set => ({
-	isSignupModalOpen: false,
-	openSignupModal: () => set({ isSignupModalOpen: true }),
-	closeSignupModal: () => set({ isSignupModalOpen: false }),
-}));
-
-export default useSignupModalStore;
