303b1f40daab689344fcd43d75b097e12402a50a
solp721 <easter721@gmail.com>
2025-07-15 00:00:27 +0900

    refactor : 현재 페이지 이동 방지

--- components/ui/universityList/UniversityList.tsx
+++ components/ui/universityList/UniversityList.tsx
@@ -1,8 +1,12 @@
 import { UniversityGrid, UniversityCard, UniversityImage } from './University.style';
 import { universities } from '@/constants/universityName';
 import { memo, useMemo } from 'react';
+import { usePathname } from 'next/navigation';
 
 const UniversityList = memo(function UniversityList() {
+  const pathname = usePathname();
+  const decodedPathname = decodeURIComponent(pathname);
+
   const universityData = useMemo(() => {
     return universities.map((university) => ({
       ...university,
@@ -12,23 +16,34 @@ const UniversityList = memo(function UniversityList() {
 
   return (
     <UniversityGrid>
-      {universityData.map((university) => (
-        <UniversityCard
-          className="university-card"
-          key={university.slug}
-          href={`/${university.slug}`}
-        >
-          <UniversityImage
+      {universityData.map((university) => {
+        const href = `/${university.slug}`;
+        const isCurrent = decodedPathname === href;
+
+        return (
+          <UniversityCard
+            className="university-card"
             key={university.slug}
-            src={university.imageSrc}
-            alt={university.name}
-            width={100}
-            height={50}
-            loading="eager"
-            priority
-          />
-        </UniversityCard>
-      ))}
+            href={href}
+            onClick={(e) => {
+              if (isCurrent) {
+                e.preventDefault();
+                e.stopPropagation();
+              }
+            }}
+          >
+            <UniversityImage
+              key={university.slug}
+              src={university.imageSrc}
+              alt={university.name}
+              width={100}
+              height={50}
+              loading="eager"
+              priority
+            />
+          </UniversityCard>
+        );
+      })}
     </UniversityGrid>
   );
 });
