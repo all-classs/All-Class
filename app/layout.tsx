c21734afc48ed8eb26107f2a97699843cef28d45
solp721 <easter721@gmail.com>
2025-07-22 15:37:51 +0900

    feat : 탠스택 쿼리 프로바이더 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import { ReactQueryProvider } from './providers';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -25,7 +26,9 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <div>{children}</div>
+        <ReactQueryProvider>
+          <div>{children}</div>
+        </ReactQueryProvider>
       </body>
     </html>
   );
