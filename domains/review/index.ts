d6a644d616ac61de1fc538003be45c84ddd31a26
solp721 <easter721@gmail.com>
2025-07-24 00:28:36 +0900

    refactor : 도메인별 서버/클라이언트 컴포넌트 구조 정리

---
+++ domains/review/index.ts
@@ -0,0 +1,7 @@
+// Review Domain - Server Components
+export { default as ReviewCard } from './server/components/review-card';
+export { default as ReviewList } from './server/components/review-list';
+
+// Review Domain - Client Components
+export { default as ReviewCardModal } from './client/components/ReviewCardModal';
+export { default as ReviewDetailModal } from './client/components/ReviewDetailModal';
