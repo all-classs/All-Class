f18f787a91805999ee15bcbaeedcd9da0cfc9560
solp721 <easter721@gmail.com>
2025-08-07 22:04:11 +0900

    refactor : 도메인 서버 컴포넌트 export 추가

--- domains/mypage/server/components/index.ts
+++ domains/mypage/server/components/index.ts
@@ -3,3 +3,5 @@ export { LectureList } from './LectureList';
 export { ReviewCard } from './ReviewCard';
 export { ReviewList } from './ReviewList';
 export { ReviewStats } from './ReviewStats';
+export { MyLectureListServer } from './MyLectureListServer';
+export { MyReviewListServer } from './MyReviewListServer';
